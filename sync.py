aadd9d3790926d40c6e362eece0390d39fb473c4
ahmadbarzak <76200661+ahmadbarzak@users.noreply.github.com>
2022-05-03 06:45:37 +1200

    Implemented JSON file reading and synchronisation for when one file is empty

--- sync.py
+++ sync.py
@@ -89,3 +89,102 @@ import json
 
 # with open(".sync.json", "w") as outfile:
 #     json.dump(dictionary, outfile)
+
+
+# step 1:
+
+#Get your two directories
+dir1 = "dir1"
+dir2 = "dir2"
+
+#check if each directory is actually valid
+isDir1 = os.path.isdir(dir1)
+isDir2 = os.path.isdir(dir2)
+
+
+#if neither directory is valid, then an error message should display
+#and the programme should stop running
+if (not isDir1) and (not isDir2):
+    print("Insert error here and exit")
+    
+#if one directory is valid but the other isn't, then the other
+#directory should be created and the files of the valid one
+#should be copied into the other directory    
+elif (not isDir1) or (not isDir2):
+    if(not isDir1):
+        empty = dir1
+        full = dir2
+    else:
+        empty = dir2
+        full = dir1
+    #make the directory
+    os.mkdir(empty)
+    
+    
+    jsonPath = os.path.join(full, ".sync.JSON")
+    open(os.path.join(empty, ".sync.JSON"), "x")
+    emptyDict = {}
+    if (not os.path.exists(jsonPath)):
+        open(jsonPath, "x")
+        fullDict = {}
+    elif os.path.getsize(jsonPath) == 0:
+            fullDict = {}
+    else:
+        with open(jsonPath) as f:
+            fullDict = json.load(f)
+    
+    
+    
+    #go through the other directory's files
+    for file in os.listdir(full):
+        if(file == ".sync.JSON"):
+            continue
+        currentPath = os.path.join(full, file)
+        #if it's a file, copy it over
+        if os.path.isfile(currentPath):
+            shutil.copy(currentPath, empty)
+            
+            #update the json file for both
+            emptyDict[file] = ["date", "time"]
+            
+            if file in fullDict:
+                fullDict[file] = ["date", "time"] + fullDict[file]
+            else:
+                fullDict[file] = ["date", "time"]
+                        
+        #if it's a directory, copy it over recursively
+        if os.path.isdir(currentPath):
+            shutil.copytree(currentPath, os.path.join(empty, file))
+    
+    with open(jsonPath, "w") as outfile:
+        json.dump(fullDict, outfile)
+    with open(os.path.join(empty, ".sync.JSON"), "w") as outfile:
+        json.dump(emptyDict, outfile)
+    
+else:
+    print("sync files")
+    print("try to merge directories, account for deletions")
+
+
+
+
+
+
+
+#Read JSON file into a dictionary to easily manipulate
+
+# with open('.sync.JSON') as f:
+#     data = json.load(f)
+    
+# print("\nfile1_1.txt:\n")
+# print(data["file1_1.txt"][0][0])
+# print(data["file1_1.txt"][0][1] + "\n")
+# print(data["file1_1.txt"][1][0])
+# print(data["file1_1.txt"][1][1])
+# print("\nfile1_2.txt:\n")
+# print(data["file1_2.txt"][0][0])
+# print(data["file1_2.txt"][0][1])
+# print("\nfile2_1.txt:\n")
+# print(data["file2_1.txt"][0][0])
+# print(data["file2_1.txt"][0][1] + "\n")
+
